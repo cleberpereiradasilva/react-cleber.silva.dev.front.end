46898bbb36372ddf3000cdb68ca511150c498d96
Cleber Silva <cleber_pesilva@yahoo.com.br>
2022-05-24 08:34:50 -0300

    chore: setup storybook interaction(tests)

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,9 @@ module.exports = {
     "@storybook/addon-interactions",
     '@storybook/addon-a11y'
   ],
+  "features": {
+    "interactionsDebugger": true
+  },
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
